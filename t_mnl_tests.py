e1fb02c0755a816cce3b831a0268edbd0f858856
phenomenelik <phenomenelik@gmail.com>
2020-03-02 12:40:22 +0100

    new tests file
    :)

---
+++ t_mnl_tests.py
@@ -0,0 +1,11 @@
+from framework.action_framework import Actions
+from pages_google import Search
+
+#todo: new tests :)
+def test_google_search(actions: Actions):
+    search_text = 'dr dissrespect'
+    search_page = Search(actions)
+    search_page.search_for(search_text)
+    assert search_text in actions.element_provider.driver.title
+    hello = actions.execute_js('return true;')
+    assert hello.lower() == 'true'
