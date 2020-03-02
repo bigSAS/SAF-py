b044c28d8196a42c8a4bdfcb84130dd468cea5fa
Tomasz Majk <bigSAS@users.noreply.github.com>
2020-03-02 16:20:56 +0100

    klaski

---
+++ learning/mnl_classes.py
@@ -0,0 +1,7 @@
+# todo: utworz klase reprezentujaca klienta w sklepie
+# ..... powinien udostepniac metode buy() -> kupuje cos
+# ..... powinien miec koszyk -> jesli koszyk jest pusty to nie moze nic kupic :)
+# napisz to tak aby z definicji klasy mozna bylo rozkminic co mozna robic
+# **** moglby miec portfel, a produkt wkladany do koszyka cene
+# **** jesli ma zamalo siana w portfelu to nie moze kupic :)
+
