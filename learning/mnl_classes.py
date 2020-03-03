8a400e2924082586faa179a2e337be85990b4cd0
phenomenelik <phenomenelik@gmail.com>
2020-03-03 19:44:06 +0100

    znowu cos nie tak

--- learning/mnl_classes.py
+++ learning/mnl_classes.py
@@ -5,27 +5,44 @@
 # **** moglby miec portfel, a produkt wkladany do koszyka cene
 # **** jesli ma zamalo siana w portfelu to nie moze kupic :)
 
+#klasa klient:
+#- powinien miec koszyk
+#- koszyk to lista stringow narazie czyli np koszyk to ['piwo', 'wino']
+#- powinien miec metode put_in_basket(item) czyli wrzucenie czegos do koszyka
+#- powinien miec metode buy() - jesli koszyk jest pusty to raise exception, jesli nie to print 'kupiles przedmioty ...'
 
 class Customer:
-    
-    
-    def __init___(self, basket, receipt, wallet):
-        self.basket = basket
-        self.receipt = receipt
-        self.wallet = wallet
-        self.got_money = wallet - receipt
+    def __init__(self, name):
+        self.name = name
+        self.basket = []
         
-    def buy_items(self):
-        if self.got_money < 0:
-            return 'Purchase refused, not enough money'
-        elif basket <= 0:
-            return 'Put items to your basket'
-        else:
-            return 'Thanks for buying good stuff. See you next time'
+    def put_in_basket(self, item):
+        self.basket.append(item)
+        
+    def buy(self):
+        if len(self.basket) == 0:
+            raise Exception('basket is empty! cannot buy')
+        print('bought:')
+        for i, item in enumerate(self.basket, 1):
+            print(f'{i}. {item}')
             
+            
+class Product:
+    def __initi__(self, prod_name, price):
+        self.prod_name = prod_name
+        self.price = price
 
-customer_1 = Customer(basket = 3, receipt = 300, wallet = 500)
+    def test(self):
+        if len(prod_name) > 0:
+            print('good')
 
-print(Customer.buy_items(customer_1))
-
-        
+if __name__ == '__main__':
+    
+#    prod_1 = Product('beer', 5)
+#    prod_1.test()
+    product.test('beer', 5)
+    customer = Customer('sas')
+    customer.put_in_basket('beer')
+    customer.put_in_basket('wine')
+    customer.put_in_basket('vodka')
+    customer.buy()
